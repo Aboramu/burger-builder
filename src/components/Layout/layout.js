dd18038c53e657ee5284170fa404dfdd88cbed74
ArT <arinside87@gmail.com>
2020-03-25 20:07:39 +0300

    before eject

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import Aux from '../../hoc';
+import classes from './layout.css';
 
 const Layout = (props) => {
+  console.log(classes.Content);
   return(
     <Aux>
       <div>
         Toolbar, SideDrawer, Backdrop
       </div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main>
     </Aux> 
