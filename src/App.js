f2eb2081c0fce39026c0bebb384a30ea9b4a7c51
ArT <arinside87@gmail.com>
2020-04-04 18:00:46 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import { Layout } from './hoc';
 import BurgerBuilder from './containers/BurgerBuilder';
+import Checkout from './containers/Checkout';
 class App extends Component {
   render() {
     return (
       <div >
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
+        <BrowserRouter>
+          <Layout>
+            <Route path='/burger-builder' component={BurgerBuilder} />
+            <Route path='/checkout' component={Checkout} />
+            <Redirect from='/' to='/burger-builder' />
+          </Layout>
+        </BrowserRouter>
       </div>
     );
   }
