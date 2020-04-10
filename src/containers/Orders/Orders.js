d641393a416a81659b47ffce3a67ecb53479ddc4
ArT <arinside87@gmail.com>
2020-04-10 21:33:29 +0300

    added fetch reducer and actions

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,47 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Order from '../../components/Order';
 import axios from '../../axios-orders';
 import { withErrorHandler } from '../../hoc';
+import * as actions from '../../store/actions';
+import Spinner from '../../components/UI/Spinner';
 
 class Orders extends Component {
-  state = {
-    orders: [],
-    loading: true
-  }
-
   componentDidMount() {
-    // получаем из firebase все записи и преобразует обект, 
-    // в массив объектов 
-    axios.get('/orders.json')
-      .then(res => {
-        const fetchOrders = [];
-        for (let key in res.data) {
-          fetchOrders.push({
-            ...res.data[key],
-            id: key
-          });
-        }
-        this.setState({loading: false, orders: fetchOrders});
-        console.log(this.state.orders)
-      })
-      .catch(err => {
-        this.setState({loading: false})
-      })
+    this.props.onFetchOrders();
   }
 
   render() {
-    return(
-      <div>
-        {this.state.orders.map( order => (
+    let orders = <Spinner />;
+    if( !this.props.loading ) {
+      orders = (
+        <div>
+        {this.props.orders.map( order => (
           <Order 
             key={order.id}
             ingredients={order.ingredients}
             price={order.price} />
         ))}
       </div>
-    );
-  }
+      );
+    }
+    return orders;
+  };
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state => {
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchOrders: () => dispatch(actions.fetchOrders())
+  };
+};
+
+export default connect(
+  mapStateToProps, mapDispatchToProps)(
+    withErrorHandler(Orders, axios)
+);
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -20,12 +20,32 @@ export const purchaseBurgerFail = (err) => {
 export const purchaseBurgerStart = () => {
   return {
     type: actionTypes.PURCHASE_BURGER_START
-  }
-}
+  };
+};
 
 export const purchaseInit = () => {
   return {
     type: actionTypes.PURCHASE_INIT
+  };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = (err) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: err
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
   };
 };
 
@@ -42,5 +62,29 @@ export const purchaseBurger = (orderData) => {
       .catch(err => {
         dispatch(purchaseBurgerFail( err ));
       });
-  }
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    // получаем из firebase все записи и преобразует обект, 
+    // в массив объектов 
+    axios.get('/orders.json')
+    .then(res => {
+      const fetchedOrders = [];
+      for (let key in res.data) {
+        fetchedOrders.push({
+          ...res.data[key],
+          id: key
+        });
+      }
+      dispatch(fetchOrdersSuccess(fetchedOrders));
+    })
+    .catch(err => {
+      dispatch(fetchOrdersFail(err));
+    });
+  };
 }
+
+
